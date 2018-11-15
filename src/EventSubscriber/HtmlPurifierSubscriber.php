4688e3ae5c28a590939800827ccfe5114c56f82a
Juampy <juampynr@gmail.com>
2018-11-15 16:59:19 +0100

    Issue #2997657 by juampynr, vijaycs85: Option to alter configuration dynamically
    Update patch

--- src/EventSubscriber/HtmlPurifierSubscriber.php
+++ src/EventSubscriber/HtmlPurifierSubscriber.php
@@ -17,21 +17,52 @@ class HtmlPurifierSubscriber implements EventSubscriberInterface {
    *   The ConfigLoaded event.
    */
   public function onConfigLoaded(ConfigLoadedEvent $event) {
-    // Define the entity_embed module's <drupal-entity> tag.
     $purifier_config = $event->getConfig();
     $def = $purifier_config->getHTMLDefinition(TRUE);
+
+    // Define the Entity Embed module's <drupal-entity> tag.
     $def->addElement(
       'drupal-entity',
       'Block',
       'Flow',
       'Common',
       [
+        'data-align' => 'CDATA',
+        'data-caption' => 'CDATA',
+        'data-cke-widget-data' => 'CDATA',
+        'data-cke-widget-upcasted' =>'CDATA',
+        'data-cke-widget-keep-attr' => 'CDATA',
         'data-embed-button' => 'CDATA',
         'data-entity-embed-display' => 'CDATA',
+        'data-entity-embed-display-settings' => 'CDATA',
         'data-entity-id' => 'CDATA',
+        'data-entity-uuid' => 'CDATA',
         'data-entity-type' => 'CDATA',
+        'data-view-mode' => 'CDATA',
+        'data-widget' => 'CDATA',
       ]
     );
+
+    // Define the URL Embed module's drupal-url element.
+    $def->addElement(
+      'drupal-url',
+      'Block',
+      'Flow',
+      'Common',
+      [
+        'data-embed-button' => 'CDATA',
+        'data-embed-url' => 'CDATA',
+        'data-label' => 'CDATA',
+        'data-url-provider' => 'CDATA',
+      ]
+    );
+
+    // Define the additional attributes that LinkIt adds to anchor tags.
+    $def->addAttribute('a', 'data-entity-href', 'URI');
+    $def->addAttribute('a', 'data-entity-substitution', 'CDATA');
+    $def->addAttribute('a', 'data-entity-title', 'CDATA');
+    $def->addAttribute('a', 'data-entity-type', 'CDATA');
+    $def->addAttribute('a', 'data-entity-uuid', 'CDATA');
   }
 
   /**
